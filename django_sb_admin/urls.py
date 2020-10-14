17306b78706a7be4375705427681c0df477cff1c
Hunt Lin <Hunt.Lin@TES-TEC.COM>
2020-10-14 12:28:14 +0800

    init

--- django_sb_admin/urls.py
+++ django_sb_admin/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView
 from django.urls import path
 
 
-
 urlpatterns = [
     url(r'^$', django_sb_admin.views.start, name='sb_admin_start'),
     path('register/', TemplateView.as_view(
@@ -19,13 +18,20 @@ urlpatterns = [
         template_name="django_sb_admin/sb_admin_404.html"),
         name='sb_admin_404'
     ),
+    path('index/', TemplateView.as_view(
+        template_name="django_sb_admin/index-new.html"),
+        name='sb_admin_index'
+    ),
     url(r'^login/$', django_sb_admin.views.login, name='sb_admin_login'),
     url(r'^dashboard/$', django_sb_admin.views.dashboard, name='sb_admin_dashboard'),
     url(r'^charts/$', django_sb_admin.views.charts, name='sb_admin_charts'),
     url(r'^tables/$', django_sb_admin.views.tables, name='sb_admin_tables'),
     url(r'^forms/$', django_sb_admin.views.forms, name='sb_admin_forms'),
-    url(r'^bootstrap-elements/$', django_sb_admin.views.bootstrap_elements, name='sb_admin_bootstrap_elements'),
-    url(r'^bootstrap-grid/$', django_sb_admin.views.bootstrap_grid, name='sb_admin_bootstrap_grid'),
-    url(r'^rtl-dashboard/$', django_sb_admin.views.rtl_dashboard, name='sb_admin_rtl_dashboard'),
+    url(r'^bootstrap-elements/$', django_sb_admin.views.bootstrap_elements,
+        name='sb_admin_bootstrap_elements'),
+    url(r'^bootstrap-grid/$', django_sb_admin.views.bootstrap_grid,
+        name='sb_admin_bootstrap_grid'),
+    url(r'^rtl-dashboard/$', django_sb_admin.views.rtl_dashboard,
+        name='sb_admin_rtl_dashboard'),
     url(r'^blank/$', django_sb_admin.views.blank, name='sb_admin_blank'),
 ]
